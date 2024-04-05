4e34995a19883312109d6d72f9811cf53bdf1007
CodeWizz <tom.heijkamp@outlook.com>
2024-04-05 12:32:05 +0200

    Added first project

---
+++ StarfishCollector/desktop/src/dev/codewizz/main/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package dev.codewizz.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import dev.codewizz.main.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Starfish Collector");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
